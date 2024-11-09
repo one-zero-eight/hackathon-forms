4ae47545991a2e1aad8477b7be268d647dbbd29e
Albert Avkhadeev <albertavkhadeev@gmail.com>
2024-11-09 15:35:37 +0300

    feat(backend): create form

--- backend/src/modules/forms/routes.py
+++ backend/src/modules/forms/routes.py
@@ -1,18 +1,27 @@
 __all__ = ["router"]
 
 
+from typing import Annotated
+
 from beanie import PydanticObjectId
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, Body, HTTPException
 
 from src.modules.forms.repository import form_repository
+from src.modules.forms.schemas import CreateFormReq
 from src.storages.mongo.forms import Form
 
 router = APIRouter(prefix="/form")
 
 
-@router.get("/form/{form_id}")
+@router.get("/{form_id}")
 async def get_form(form_id: PydanticObjectId) -> Form:
     form = await form_repository.get(form_id)
     if not form:
         raise HTTPException(status_code=404)
+    return form
+
+
+@router.post("/")
+async def create_form(req: Annotated[CreateFormReq, Body(embed=True)]) -> Form:
+    form = await form_repository.create(req)
     return form
