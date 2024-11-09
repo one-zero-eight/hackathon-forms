6e2597c86cd26e900ad614abe98b8013a6e8d645
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2024-11-09 11:03:44 +0300

    add tanstack query

---
+++ frontend/app/_providers.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {PropsWithChildren} from "react";
+
+// Create a global query client
+const queryClient = new QueryClient()
+
+/**
+ * Client providers for React components.
+ */
+export function Providers({children}: PropsWithChildren) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  )
+}
